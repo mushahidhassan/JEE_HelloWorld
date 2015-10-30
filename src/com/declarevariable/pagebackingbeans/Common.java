a94b8c4364906087cb2ef77398e99a9360f34e7b
mushahidhassan <mushahidhassan110@gmail.com>
2015-10-30 18:55:40 +0500

    if condition

---
+++ src/com/declarevariable/pagebackingbeans/Common.java
@@ -0,0 +1,11 @@
+package com.declarevariable.pagebackingbeans;
+
+/**
+ * Created by m on 10/30/2015.
+ * This is a utility class
+ */
+public class Common {
+    public static class Var{
+        public  static String name = "Mushahid";
+    }
+}
